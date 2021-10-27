183b857c66083f1e3737102a1a673c72aaab2266
JessicaChouha <62106107+JessicaChouha@users.noreply.github.com>
2021-10-27 11:40:56 -0400

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,21 @@
+let age=25;
+let year=2019;
+
+console.log(age, year);
+
+age=30;
+console.log(age);
+
+const points =100;
+consol.log(points);
+
+var score=75;
+consol.log(score);
+
+const password= 'password1234';
+if(password.length >= 12 && password.include('@')){
+  console.log('that pass is mighty strong');
+} else{
+  console.log('password is not long enough'); 
+}
+}
